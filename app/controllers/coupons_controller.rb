b3ecbbadee8dab7c469c120d5e8b777d7a951818
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-25 20:54:07 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,20 @@
+class CouponsController < ApplicationController
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  def show
+    @coupon = Coupon.find_by_id(params[:id])
+  end
+
+  def index
+    @coupons = Coupon.all
+  end
+end
